6d6b97a2a3ed430958525dc78818f0573f57b513
user140592 <user140592@eu201.local>
2019-02-16 20:17:40 +0000

    text

--- AutoTextViewSizing/ViewController.swift
+++ AutoTextViewSizing/ViewController.swift
@@ -12,9 +12,46 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let textView = UITextView()
+        textView.frame = CGRect(x: 0, y: 0, width: 200, height: 100)
+        textView.backgroundColor = .lightGray
+        
+        view.addSubview(textView)
+
+        //use auto layout to set my textView
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        [
+            textView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            textView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            textView.heightAnchor.constraint(equalToConstant: 50)
+        ].forEach({$0.isActive = true})
+        
+        textView.font = UIFont.preferredFont(forTextStyle: .headline)
+        
+        textView.delegate = self
+        textView.isScrollEnabled = false
+        
+        textViewDidChange(textView)
     }
 
 
+}
+
+extension ViewController: UITextViewDelegate {
+    
+    func textViewDidChange(_ textView: UITextView) {
+        print(textView.text)
+        let size = CGSize(width: view.frame.width, height: .infinity)
+        let estimatedSize = textView.sizeThatFits(size)
+        
+        textView.constraints.forEach {(constraint) in
+            if constraint.firstAttribute == .height {
+                constraint.constant = estimatedSize.height
+            }
+        }
+    }
 }
 
